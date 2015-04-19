1bcf71d3c97408e97c204a1524c9335845d5905b
kdo <otwmoo@gmail.com>
2015-04-20 07:09:28 +0900

    init

---
+++ MqTest/src/casestudy/case2/durability/Worker.java
@@ -0,0 +1,49 @@
+package casestudy.case2.durability;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+
+public class Worker {
+
+	private final static String QUEUE_NAME = "hello";
+
+	public static void main(String[] argv) throws java.io.IOException,
+			java.lang.InterruptedException {
+
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+		QueueingConsumer consumer = new QueueingConsumer(channel);
+		boolean autoAck = false;
+		channel.basicConsume("hello", autoAck, consumer);
+
+		
+		while (true) {
+			QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+			String message = new String(delivery.getBody());
+
+			System.out.println(" [x] Received '" + message + "'");
+			doWork(message);
+			channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+//			System.out.println(" [x] Done");
+		}
+
+	}
+
+	private static void doWork(String task) throws InterruptedException {
+		for (char ch : task.toCharArray()) {
+			if (ch == '.') {
+				Thread.sleep(1000);
+				System.out.print(ch);
+			}
+		}
+	}
+
+}
